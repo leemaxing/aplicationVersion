b0e34acccaf9ab87da333030fa28c064350731bf
limaoxiang@liicon <hjolmx@163.com>
2020-11-16 10:23:32 +0800

    不是用androidX,解决内容提供者冲突,修改更新页面显示高度,更换图片2

--- appVersion/src/main/java/com/max/appversion/update/UpdateTools.java
+++ appVersion/src/main/java/com/max/appversion/update/UpdateTools.java
@@ -79,8 +79,8 @@ public class UpdateTools {
         String packgeName = mContext.getApplicationInfo().packageName;
         String api = "http://liicon.com:7768/updateMsg/getMsg";
         Map param = new HashMap();
-//        param.put("appName","liicon.com.hzspotcheck");
-        param.put("appName",packgeName);
+        param.put("appName","liicon.com.hzspotcheck");
+//        param.put("appName",packgeName);
 
         NetworkingTools.post(api, param, new NetworkingTools.NetworkingResponseListener() {
             @Override
