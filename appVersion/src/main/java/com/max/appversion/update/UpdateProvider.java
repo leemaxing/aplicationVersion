98554afbe8906ad928aa516fcbd09c7aa2066bc3
leemaxing <hjolmx@163.com>
2021-03-31 18:11:44 +0800

    更换内容提供者

---
+++ appVersion/src/main/java/com/max/appversion/update/UpdateProvider.java
@@ -0,0 +1,9 @@
+package com.max.appversion.update;
+
+import android.support.v4.content.FileProvider;
+
+/*
+ *create by lmx  2021/3/31:6:10 PM
+ */
+public class UpdateProvider  extends FileProvider {
+}
