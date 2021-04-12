427cd7c9610bdd6bf38b2daedcc111fa81840cda
leemaxing <hjolmx@163.com>
2021-04-12 16:00:30 +0800

    增加请求版本方法

--- appVersion/src/main/java/com/max/appversion/AppVersion.java
+++ appVersion/src/main/java/com/max/appversion/AppVersion.java
@@ -130,6 +130,74 @@ public class AppVersion implements ActivityOnResultListenner {
     }
 
 
+    public void checkCurrentVesion(){
+        updateTools.checkVersion(new UpdateTools.UpdateToolsCallBack() {
+            @Override
+            public void needUpdate(String msg, final boolean must, String serverVersion) {
+                final String  message = msg;
+                mContext.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        PackageManager manager = mContext.getPackageManager();
+                        PackageInfo info = null;
+                        try {
+                            info = manager.getPackageInfo(mContext.getPackageName(), 0);
+                        } catch (PackageManager.NameNotFoundException e) {
+                            e.printStackTrace();
+                        }
+                        String version = info.versionName;
+                        String newMsg = message.replace("#必须更新#","");
+                        newMsg = "版本更新：当前版本" + version + "\n" + newMsg;
+
+                        updateDialog.setMustUpdate(must);
+                        updateDialog.setMsg(newMsg);
+                        updateDialog.show();
+
+                        updateDialog.setDialogClicklistener(new UpdateDialog.ChooseDialogClicklistener() {
+                            @Override
+                            public void onFirstButtonClick() {
+                                updateDialog.dismiss();
+                            }
+
+                            @Override
+                            public void onSecButtonClick() {
+                                updateDialog.setInUpdating(true);
+                                updateTools.getDowLoadToken();
+                            }
+
+                            @Override
+                            public void onCancel() {
+
+                            }
+                        });
+                    }
+                });
+            }
+
+            @Override
+            public void onNoUpdate() {
+
+            }
+
+            @Override
+            public void onDownLoadProgress(int progress) {
+                updateDialog.setProgress(progress);
+            }
+
+            @Override
+            public void onDownLoaded(String path) {
+                updateDialog.dismiss();
+                ToolClass.mainTread(new ToolClass.ToolMainThreadCall() {
+                    @Override
+                    public void call() {
+                        installUtil.install();
+                    }
+                });
+            }
+        });
+    }
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == InstallUtil.UNKNOWN_CODE){
